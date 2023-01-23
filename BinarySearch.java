52a8cad6e33156b6f2ebd028a2447a048875baa9
Justin Ramirez <ramirez.justin@gmail.com>
2023-01-22 19:01:07 -0500

    Completed Lab 2 for submission

--- BinarySearch.java
+++ BinarySearch.java
@@ -2,3 +2,38 @@
 //CSC 2720 Lab CS225
 //Lab time: W 1300
 //Due time: 1/22
+
+import java.util.*;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int a[] = { 11, 21, 33, 40, 50 };
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter a number to search for: ");
+        int n = sc.nextInt();
+        int index = binarySearch(a, n);
+        if (index >= 0)
+            System.out.println("The index of " + n + " is: " + index);
+        sc.close();
+    }
+
+    public static int binarySearch(int v[], int n) {
+        int left = 0;
+        int right = v.length - 1;
+        int count = 0;
+        while (left <= right) {
+            int mid = left + (right - left)/2;
+            count++;
+            if(v[mid] == n) {
+                System.out.println("Found input number, search required " + count + " check(s).");
+                return mid;
+            } else if (v[mid] < n) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        System.out.println("Failed to find the input number.");
+        return -1;
+    }
+}
--- DeDuplication.java
+++ DeDuplication.java
@@ -2,18 +2,20 @@
 //CSC 2720 Lab CS225
 //Lab time: W 1300
 //Due time: 1/22
-import java.util.*;
 
-class DeDuplication {
+public class DeDuplication {
     public static void main(String[] args){
         int a[] = { 50, 11, 33, 21, 40, 50, 40, 40, 21 };
-        //Sort Array
+        // Sort Array
         sortArray(a);
+        // Remove duplicates
         int b[] = removeDuplicates(a);
+        // Print Array
         printArray(b);
           
     }
 
+    // My method for sorting the array
     public static void sortArray(int[] a){
         int n = a.length;
         int tmp;
@@ -28,6 +30,7 @@ class DeDuplication {
         }
     }
 
+    // My method for removing the duplicates
     public static int[] removeDuplicates(int[] arr) {
         int[] newArr = new int[arr.length];
         int k = 0;
@@ -43,10 +46,14 @@ class DeDuplication {
                 newArr[k++] = arr[i];
             }
         }
-        return Arrays.copyOf(newArr, k);
+        int[] b = new int[k];
+        for (int i = 0; i < (k); i++){
+            b[i] = newArr[i];
+        }
+        return b;
     }
-    
 
+    // Method for printing the array
     public static void printArray(int[] a){
         for(int i = 0; i < a.length; i++){
             System.out.print(a[i] + " ");
