5328b9b2928fb9f62f1c38c46119c733fee2b5bd
Justin Ramirez <ramirez.justin@gmail.com>
2023-01-27 13:27:51 -0500

    small edit

--- CountingSort.java
+++ CountingSort.java
@@ -12,26 +12,26 @@ public class CountingSort {
 
     // Method to sort array using counting sort
     public static void countingSort(int[] x) {
-        int max = 0;
-        for (int i = 0; i < x.length; i++) {
-            if (x[i] > max) {
-                max = x[i];
+        int max = 0;   // 1 step
+        for (int i = 0; i < x.length; i++) {   // n time
+            if (x[i] > max) {   // 1 step
+                max = x[i];   // 1 step
             }
         }
 
-        int[] count = new int[max + 1];
-        for (int i = 0; i < x.length; i++) {
-            count[x[i]]++;
+        int[] count = new int[max + 1];   // 1 step
+        for (int i = 0; i < x.length; i++) {   // n time
+            count[x[i]]++;   // 1 step
         }
 
-        int j = 0;
-        for (int i = 0; i <=max; i++) {
-            while (count[i] > 0) {
-                x[j++] = i;
-                count[i]--;
+        int j = 0;   // 1 step
+        for (int i = 0; i <=max; i++) {   // n time
+            while (count[i] > 0) { // log(n) times
+                x[j++] = i;   // 1 step
+                count[i]--;   // 1 step
             }
         }
-    }
+    }   // 3 + 3n + 2n*log(n) -> O(n*log(n))
 
     // Method to print the new array
     public static void printArray(int[] x){
