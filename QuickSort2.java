c4ddda433c4989e85c907b6175e80c539fb4abcd
Justin Ramirez <ramirez.justin@gmail.com>
2023-02-18 17:35:16 -0500

    Making random changes and learning things

--- QuickSort2.java
+++ QuickSort2.java
@@ -1,50 +1,9 @@
-/* Your Name
- * CSc 2720 Lab #6
- * Lab time: W1300
- * Due time: 02/19/2023 @ 2359
-*/
-public class QuickSort {
-
-    /* Test Cases:
-     * Empty arrays
-     * Switched array 1 and 2
-     * Tested arrays of very different lengths
-     * A varied number of duplicates
-     * No matches
-     * Unsorted arrays
-     */    
+public class QuickSort2 {
     public static void main(String[] args) {
-        int[] nums = { 50, 11, 33, 21, 40, 50, 40, 40, 21 };
-        quickSort(nums, 0, nums.length-1);
-        nums = removeDuplicates(nums, nums.length);
-        printArray(nums);
-    }
-
-    // This method solves the lab as requested in O(n log2(n)) time complexity
-    // it uses the partition method to split the array
-    public static void quickSort(int[] arr, int low, int high) {
-        if (low < high){
-            int pivotIndex = partition(arr, low, high);
-            quickSort(arr, low, pivotIndex-1);
-            quickSort(arr, pivotIndex+1, high);
-        }
-    }
-
-    public static int partition(int[] arr, int low, int high) {
-        int pivot = arr[high];
-        int pi = low-1;
-        for (int i = low; i <= high-1; i++) {   //   O(n)
-            if (arr[i] < pivot) {
-                pi++;
-                int temp = arr[pi];
-                arr[pi] = arr[i];
-                arr[i] = temp; 
-            }
-        }
-        int temp = arr[pi+1];
-        arr[pi+1] = arr[high];
-        arr[high] = temp;
-        return (pi+1);
+        int[] arr = { 1, 1, 4, 7, 22, 56, 44, 55, 1, 55, 60, 45 };
+        arr = quickSort2(arr);
+        arr = removeDuplicates(arr);
+        printArray(arr);
     }
 
     // This is a different method I found in a book I was reading,
@@ -86,20 +45,13 @@ public class QuickSort {
         return result;
     }
 
-    public static void printArray(int[] x){
-        for (int i = 0; i < x.length; i++) {
-            System.out.print(x[i] + " ");
-        }
-        System.out.println();
-    }
-
-    public static int[] removeDuplicates(int[] arr, int n) {
-        if (arr == null || arr.length == 0) {
+    public static int[] removeDuplicates(int[] arr) {
+        if (arr.length == 0 || arr == null)
             return arr;
-        }
+        int n = arr.length;
         int j = 0;
-        for (int i = 0; i < arr.length; i++) {  // O(n) time
-            if (arr[i] != arr[j]) {
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != arr[j]) { // compares pointer for j to a moving pointer for i, they are different it brings i to the next spot
                 j++;
                 arr[j] = arr[i];
             }
@@ -109,5 +61,14 @@ public class QuickSort {
             newArr[i] = arr[i];
         }
         return newArr;
+    }
+
+    public static void printArray(int[] arr) {
+        if (arr.length == 0)
+            System.out.println("The array is empty!");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
 }
---
+++ myName.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("My name is Justin Ramirez\n");
+    return 0;
+}
