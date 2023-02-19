265950d6a468988936b556a7d9d5a93f1ffcc9a0
Justin Ramirez <ramirez.justin@gmail.com>
2023-02-18 23:43:12 -0500

    small changes

--- QuickSort.java
+++ QuickSort.java
@@ -47,45 +47,6 @@ public class QuickSort {
         return (pi+1);
     }
 
-    // This is a different method I found in a book I was reading,
-    public static int[] quickSort2(int[] arr){
-        if (arr.length < 2) // base case
-            return arr;
-        int pivot = arr[0]; // Recursive case
-        int low = 0;
-        int high = 0;
-        int[] less = new int[arr.length]; // Sub-array of all elements less than pivot
-        int[] greater = new int[arr.length]; // Sub-array of all elements greater than pivot
-        for (int i = 1; i < arr.length; i++){
-            if (arr[i] <= pivot)
-                less[low++] = arr[i]; // adds elements less than or equal to pivot to less
-            else 
-                greater[high++] = arr[i]; // adds elements greater than pivot to greater
-        }
-        // Create shorter copies of the less and greater arrays
-        int[] sortedLess = new int[low];
-        int[] sortedGreater = new int[high];
-        for (int i = 0; i < low; i++) {
-            sortedLess[i] = less[i];
-        }
-        for (int i = 0; i < high; i++) {
-            sortedGreater[i] = greater[i];
-        }
-        // run them back through the quicksort to properly sort them
-        sortedLess = quickSort2(sortedLess);
-        sortedGreater = quickSort2(sortedGreater);
-        // Create new array with proper length
-        int[] result = new int[low + 1 + high];
-        for (int i = 0; i < low; i++) {
-            result[i] = sortedLess[i];
-        }
-        result[low] = pivot;
-        for (int i = 0; i < high; i++) {
-            result[low + 1 + i] = sortedGreater[i];
-        }
-        return result;
-    }
-
     public static void printArray(int[] x){
         for (int i = 0; i < x.length; i++) {
             System.out.print(x[i] + " ");
