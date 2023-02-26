89bfb471d9da5ecd00e3ef18429c245e12f65aa1
Justin Ramirez <ramirez.justin@gmail.com>
2023-02-26 14:16:38 -0500

    Added linked LinkedList class

---
+++ LinkedList.java
@@ -0,0 +1,25 @@
+//Justin Ramirez
+//CSC 2720 Lab CS225
+//Lab time: W 1300
+//Due time: 2/12/2023 @ 11:59PM
+
+public class LinkedList {
+    Node head;
+    static class Node {
+        int val;
+        Node next;
+        // Create a node
+        Node(int val) { 
+        this.val = val; 
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList node1 = new LinkedList();
+        LinkedList node2 = new LinkedList();
+    }
+
+    void printNthFromLast(){
+
+    }
+}
