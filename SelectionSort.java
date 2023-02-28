8de2cf2873be7c702cfb9a53aa7cda602c2b215c
Justin Ramirez <ramirez.justin@gmail.com>
2023-02-28 00:04:26 -0500

    small changes

--- SelectionSort.java
+++ SelectionSort.java
@@ -6,30 +6,35 @@
 public class SelectionSort {
     public static void main(String[] args) {
         int[] nums = {50, 11, 33, 21, 40, 50, 40, 40, 21};
-        selectionSort(nums);
+        selectionSort(nums, nums.length);
         printArray(nums);
     }
 
-    // My method for selsction sort
-    public static void selectionSort(int arr[]) {   // input size 1
-        int n = arr.length;
-  
+    // My method for selection sort, Selection sort iterates through the array n^2 times
+    // finding the minimum element in the array and bringing it to the front of the 
+    // unsorted portion
+    public static void selectionSort(int arr[], int n) {   // input size 1  
         // One by one move boundary of unsorted subarray
         for (int i = 0; i < n-1; i++) {
-            // Find the minimum element in unsorted array
-            int min_idx = i;
+            int min_idx = i;        // Find the minimum element in unsorted array
             for (int j = i + 1; j < n; j++){
                 if (arr[j] < arr[min_idx])
                     min_idx = j;
             }
-            // Swap the found minimum element with the first
-            // element
-            int temp = arr[min_idx];
-            arr[min_idx] = arr[i];
-            arr[i] = temp;
+            // Swap the found minimum element with the first element
+            swap(arr, i, min_idx);
         }
     }
 
+    // My method for swapping two elements of an array
+    public static void swap(int arr[], int i, int min_idx) {
+        int temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+
+    }
+
+    // My method for printing all the elements of an array
     public static void printArray(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
