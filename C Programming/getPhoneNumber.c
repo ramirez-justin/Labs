87f8f1731d98704e1869b64dc47d800d5f0561db
Justin Ramirez <ramirez.justin@gmail.com>
2023-03-05 19:15:12 -0500

    added some new assignments

--- C Programming/getPhoneNumber.c
+++ C Programming/getPhoneNumber.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
-    float i;
-    int x, y;
+    int a1, a2, a3;
     printf("Enter a phone number in the form (XXX-XXX-XXX): ");
-    scanf("%d-%f-%d", &x, &i, &y);
-    printf("You entered (%d)%f-%d", x, i, y);
+    scanf("%d-%d-%d", &a1, &a2, &a3);
+    printf("You entered: (%03d)%03d-%04d", a1, a2, a3);
     return 0;
 }
---
+++ QueueWithMax.java
@@ -0,0 +1,59 @@
+import java.util.*;
+/* Test Cases: 
+ * Handling empty Queue
+ * Queue with duplicates
+ * Tested queues of very different lengths
+ * A varied number of duplicates
+ * Try/catch block to handle improper input
+ * If statement to handle improper mode selection input
+*/
+public class QueueWithMax {
+    public static void main(String[] args){
+        // Created two new objects, mainQueue and maxQueue
+        ArrayDeque<Integer> mainQueue = new ArrayDeque<>();
+        ArrayDeque<Integer> maxQueue = new ArrayDeque<>();
+        // Created two scanners, one to track mode and one to recieve input data
+        Scanner charScanner = new Scanner(System.in);
+        Scanner dataScanner = new Scanner(System.in);
+        // Character for mode selection
+        char type;
+        do {
+            type = ' ';     // initialize/reset mode selection
+            System.out.println("Please enter an operation character(i = insert, r = remove, q = quit):");
+            type = charScanner.next().charAt(0);    //input for mode selection
+            if (type == 'i') {      // input mode
+                try {       // needed a try/catch for posible input type errors
+                // input mode prompt to add numbers to queue
+                    System.out.println("Please enter a number to add to the queue: ");
+                    int current = dataScanner.nextInt();
+                    mainQueue.add(current);
+                // adding the max element to the maxQueue
+                    while (!maxQueue.isEmpty() && maxQueue.peekLast() < current) {
+                        maxQueue.removeLast();      // removing any elements at the end of maxQueue less than current
+                    }
+                    maxQueue.addLast(current); // adding current value to max queue
+                } catch (Exception e) {     // catch block for handleing improper input types
+                    System.out.println("Error: improper input!");
+                    dataScanner.next();
+                }
+            } else if (type == 'r') {   // remove mode
+                if (mainQueue.isEmpty())    // to prevent a NoSuchElementException
+                    continue;
+                int current = mainQueue.remove();   // Removes first element in queue
+                if (current == maxQueue.peekFirst()) {  // check if current is first element of maxQueue and removes it
+                    maxQueue.removeFirst();
+                }
+                System.out.println("The number " + current + " has been removed" );
+  
+            } else if (type == 'q'){    //quit mode
+                break;
+            } else {
+                System.out.println("Invalid input. Please try again."); // handles improper input so mode selection
+            }
+            System.out.println("Main Queue: " + mainQueue);     // Prints mainQueue
+            System.out.println("Max Queue: " + maxQueue);       // Prints maxQueue
+        } while(type != 'q');      
+        charScanner.close();
+        dataScanner.close();
+    }
+}
