e5d06c4e3c85fa91718de97f6f2aadd7d77a100c
Justin Ramirez <ramirez.justin@gmail.com>
2023-03-26 17:03:00 -0400

    Small changes, added new projects

--- C Programming/listLetterFreq.c
+++ C Programming/listLetterFreq.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
-#include <ctype.h>
+
+#define NUM_LETTERS 26
 
 int main() {
-    char str[250];
-    int freq[26] = {0}; // initialize frequency array to 0
+    int freq[NUM_LETTERS] = {0}; // initialize frequency array to 0
 
-    printf("Enter a string: ");
-    fgets(str, sizeof(str), stdin); // read input string
-
-    // count frequency of each letter, ignoring case
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (isalpha(str[i])) {
-            char c = tolower(str[i]);
+    int c;
+    while ((c = getchar()) != EOF) {
+        if (c >= 'a' && c <= 'z') {
             freq[c - 'a']++;
+        } else if (c >= 'A' && c <= 'Z') {
+            freq[c - 'A']++;
         }
     }
 
     // print frequency of each letter
-    for (int i = 0; i < 26; i++) {
-        printf("Letter %c or %c appears %d times\n", 'a' + i, 'A' + i, freq[i]);
+    for (int i = 0; i < NUM_LETTERS; i++) {
+        printf("\nLetter %c or %c appears %d times", 'a' + i, 'A' + i, freq[i]);
     }
 
     return 0;
---
+++ Java Programming/MyBinaryTree.java
@@ -0,0 +1,84 @@
+// Justin Ramirez
+// CSC 2720 Lab 10
+// Class Time: TH 1415
+// Due time: 3/26/2023 @ 2359
+
+public class MyBinaryTree {
+/* Test Cases: 
+ * Handling empty BinaryTree
+ * Tree with duplicates
+ * Tested Trees of different sizes
+ */
+    public static void main(String[] args) {
+        MyBinaryTree tree = new MyBinaryTree(); //Create instance of Binary Tree
+        tree.root = new Node(4);    //Adding new nodes at respective locations in tree
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(6);
+        tree.root.left.left = new Node(1);
+        tree.root.left.right = new Node(3);
+        tree.root.right.left = new Node(5);
+        tree.root.right.right = new Node(7);
+
+        System.out.print("Pre-order traversal: ");   // Print each method for travesing tree      
+        tree.preOrder(tree.root);
+
+        System.out.print("\nIn-order traversal: ");
+        tree.inOrder(tree.root);
+
+        System.out.print("\nPost-order traversal: ");
+        tree.postOrder(tree.root);
+    }
+
+    // Class to represent Tree node 
+    static class Node {
+        int data;
+        Node left, right;
+
+        public Node(int item) {
+            data = item;
+            left = null;
+            right = null;
+        }
+    }
+
+    Node root;
+
+    /** 
+     * Pre-order traversal of the binary tree
+     * Root - Left - Right
+     * @param
+     */
+    public void preOrder(Node node) {
+        if (node == null)
+            return;
+        System.out.print(node.data + " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    /**
+     * In-order traversal of the binary tree
+     * Left - Root - Right
+     * @param 
+     */
+    public void inOrder(Node node) {
+        if (node == null)
+            return;
+        inOrder(node.left);
+        System.out.print(node.data + " ");
+        inOrder(node.right);
+    }
+
+    /** 
+     * Post-order traversal of the binary tree
+     *  Left - Right - Root
+     * @param 
+     */
+    public void postOrder(Node node) {
+        if (node == null)
+            return;
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.data + " ");
+    }
+}
